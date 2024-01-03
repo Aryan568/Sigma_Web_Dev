cc1fa5b8c6cc822d5814dd970a8049f96e62b148
Aryan568 <aryanpatidar730@gmail.com>
2024-01-03 22:38:59 +0530

    learning javascript

--- script.js
+++ script.js
@@ -1,2 +1,8 @@
 // alert("Welcome to Sigma Web Development Course")
 console.log("Hello World")
+var a= prompt("What is your name?")
+
+if(a==="Aryan"){
+    console.log("Valid User")
+}
+else console.log("Invalid User")
